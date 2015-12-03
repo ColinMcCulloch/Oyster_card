ad8bbd74edbee7ed4eda36e3ba5a7ec498f54f76
Colin McCulloch <mccolane@gmail.com>
2015-12-03 19:07:32 +0000

    Pt.13 Refactor

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,80 +1,77 @@
 require 'oystercard'
+
 describe Oystercard do
-  let(:oystercard) { described_class.new }
+  subject(:oystercard) { described_class.new }
   let(:entry_station) {double :entry_station}
   let(:exit_station) {double :exit_station}
-   describe '#balance' do
-     it 'returns balance as 0' do
-       expect(oystercard.balance).to eq (0)
-     end
-   end
- describe '#top_up' do
+  LIMIT = Oystercard::LIMIT
+  MIN_FARE = Oystercard::MINIMUM_FARE
 
-    it 'adds cash to the oystercard' do
-      expect { oystercard.top_up(10) }.to change{ oystercard.balance }.by 10
+  context 'card WITHOUT money' do
+    describe '#balance' do
+      it 'returns balance as 0' do
+        expect(oystercard.balance).to eq (0)
+      end
     end
-    it 'prevent balance from exceeding £90' do
-      over_90 = "Balance cannot exceed £#{Oystercard::LIMIT}"
-      oystercard.top_up(90)
-      expect {oystercard.top_up(1)}.to raise_error over_90
+    describe '#top_up' do
+      it 'adds cash to the oystercard' do
+        expect { oystercard.top_up(10) }.to change{ oystercard.balance }.by 10
+      end
+      it 'prevents balance from exceeding £90' do
+        over_90 = "Balance cannot exceed £#{LIMIT}"
+        oystercard.top_up(90)
+        expect {oystercard.top_up(1)}.to raise_error over_90
+      end
     end
- end
-
- describe '#in_journey?' do
-
-  it "Should return nil before touching in" do
-    expect(oystercard).not_to be_in_journey
+    describe '#touch_in' do
+      it 'raises an error if balance is less than £1' do
+        insufficient_funds = "Insufficient funds: Please add top up"
+        expect{oystercard.touch_in(entry_station)}.to raise_error insufficient_funds
+      end
+    end
   end
-
-  it 'Passing touch_in should change in Journey to true' do
-    oystercard.top_up(Oystercard::LIMIT)
-    oystercard.touch_in(entry_station)
-    expect(oystercard).to be_in_journey
+  context 'card WITH money' do
+    before do
+      oystercard.top_up(LIMIT)
+    end
+    describe '#in_journey?' do
+      it "should return false before touching in" do
+        expect(oystercard).not_to be_in_journey
+      end
+      it 'should return true after touching in' do
+        oystercard.touch_in(entry_station)
+        expect(oystercard).to be_in_journey
+      end
+    end
+    describe '#touch_out' do
+      it 'should deduct the minimum fare' do
+        oystercard.touch_in(entry_station)
+        expect { oystercard.touch_out(exit_station) }.to change { oystercard.balance }.by -MIN_FARE
+      end
+      describe '#in_journey?' do
+        it 'should return false' do
+          oystercard.touch_in(entry_station)
+          oystercard.touch_out(exit_station)
+          expect(oystercard).not_to be_in_journey
+        end
+      end
+    end
+    describe '#history' do
+      it 'is empty by default' do
+        expect(oystercard.history).to eq ({})
+      end
+      it 'stores Journey history' do
+        oystercard.touch_in("Brixton")
+        oystercard.touch_out("Victoria")
+        expect(oystercard.history).to eq ({J1: ["Brixton", "Victoria"]})
+      end
+      it 'stores multiple journeys in history' do
+        oystercard.touch_in("Brixton")
+        oystercard.touch_out("Victoria")
+        oystercard.touch_in("Stockwell")
+        oystercard.touch_out("Pimlico")
+        expect(oystercard.history).to eq ({J1: ["Brixton", "Victoria"], J2: ["Stockwell", "Pimlico"]})
+      end
+    end
   end
-  it 'Passing touch_out should change in_Journey to false' do
-    oystercard.top_up(Oystercard::LIMIT)
-    oystercard.touch_in(entry_station)
-    oystercard.touch_out(exit_station)
-    expect(oystercard).not_to be_in_journey
-  end
-end
-describe '#touch_in' do
-  it 'Raise an error when touching in if balance is less than £1' do
-    insufficient_funds = "Insufficient funds: Please add top up"
-    expect{oystercard.touch_in(entry_station)}.to raise_error insufficient_funds
-  end
-
-end
-
-describe '#touch_out' do
-  it 'Should deduct the minimum fare on touch_out' do
-    oystercard.top_up(Oystercard::LIMIT)
-    oystercard.touch_in(entry_station)
-    expect { oystercard.touch_out(exit_station) }.to change { oystercard.balance }.by -Oystercard::MINIMUM_FARE
-  end
-
-end
-
-describe '#history' do
-  it 'history is emty by default' do
-    expect(oystercard.history).to eq ({})
-  end
-  it 'stores Journey history' do
-    oystercard.top_up(30)
-    oystercard.touch_in("Brixton")
-    oystercard.touch_out("Victoria")
-    expect(oystercard.history).to eq ({J1: ["Brixton", "Victoria"]})
-  end
-  it 'Stores multiple journeys in history' do
-    oystercard.top_up(30)
-    oystercard.touch_in("Brixton")
-    oystercard.touch_out("Victoria")
-    oystercard.touch_in("Stockwell")
-    oystercard.touch_out("Pimlico")
-    expect(oystercard.history).to eq ({J1: ["Brixton", "Victoria"], J2: ["Stockwell", "Pimlico"]})
-  end
-end
-
-
-
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,11 +1,14 @@
 require 'station'
-describe Station do
-  let(:station) { described_class.new('Brixton', '2') }
 
-  it 'creates a station with a name' do
-    expect(station.name).to eq 'Brixton'
-  end
-  it 'creates a station with a zone' do
-    expect(station.zone).to eq '2'
+describe Station do
+  subject(:station) { described_class.new('Brixton', '2') }
+
+  describe 'initialize' do
+    it 'creates a station with a name' do
+      expect(station.name).to eq 'Brixton'
+    end
+    it 'creates a station with a zone' do
+      expect(station.zone).to eq '2'
+    end
   end
 end
